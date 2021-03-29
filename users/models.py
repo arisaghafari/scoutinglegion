f17f3918cda176d0df065dad8e81254a9e6af3f8
Yasinaskarian <askarian98@gmail.com>
2021-03-29 18:54:51 +0430

    add project with login/sign-up

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(_('username'), max_length=100, null=False, unique=True)
+    firstname = models.CharField(_('first name'), max_length=100, null=True)
+    lastname = models.CharField(_('last name'), max_length=100, null=True)
+    city = models.CharField(_('city of residence'), max_length=100, null=False)
+    profile_picture = models.ImageField(upload_to='Uploaded/userprofile', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
