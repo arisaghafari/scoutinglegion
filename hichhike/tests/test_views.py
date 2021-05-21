4fb5008b1518a5e7f357e4f4327964357c2fe224
Yasinaskarian <askarian98@gmail.com>
2021-05-21 19:05:56 +0430

    "add tests"

---
+++ hichhike/tests/test_views.py
@@ -0,0 +1,43 @@
+import json
+from django.test import TestCase, Client
+from Locations.models import Location
+from django.urls import reverse
+from users.models import CustomUser
+from hichhike.models import Hichhike
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email="test1@gmail.com", password='1234')
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='token ' + self.token.key)
+
+    def test_creator_locations_GET(self):
+        response = self.client.get(reverse('creator_hichhikes'), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.user.creator_location.count(), 0)
+        self.assertEqual(response.content, b'[]')
+        data = {
+            "creator_type": "d",
+            "creator_gender": "m",
+            "creator_age": 22,
+            "source": "shiraz",
+            "destination": "tehran",
+            "fellow_traveler_num": 4,
+            "description": "string",
+            "cities": ["abadeh", "esfahan"]
+        }
+        self.client.generic(method="POST", path=reverse('create_hichhike'), data=json.dumps(data),
+                                   content_type='application/json')
+        response = self.client.get(reverse('creator_hichhikes'), content_type='application/json')
+        self.assertEqual(self.user.creator_hichhike.count(), 1)
+        self.assertNotEqual(response.content, b'[]')
+
+        # print(response.content)
+
+
+
+
