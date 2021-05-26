73c5df800e6a9e721b8daf742fce45fc32feba35
arisaghafari <arisaghafari68@gmail.com>
2021-05-26 09:36:38 +0430

    fix some problem

--- Locations/urls.py
+++ Locations/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('creator_locations/', ViewLocationViewSet.as_view(), name='creator_locations'),
     path('location_detail/<slug:id>', GetLocationDetails.as_view()),
     path('get_locations/', AllLocations.as_view()),
-    # path('search_by_name/<slug:name>', SearchByName.as_view()),
     re_path(r'search_by_name/(?P<name>[-\w|\W]+)/', SearchByName.as_view()),
-    # ?P<name>[\w|\W]+
+    path('getCategory/', get_all_categories),
 ]
