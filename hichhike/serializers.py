2fecc9c58238f252ffdc67447f30033a46cf949e
Yasinaskarian <askarian98@gmail.com>
2021-07-10 23:34:42 +0430

    "add fields to hichike serializers"

--- hichhike/serializers.py
+++ hichhike/serializers.py
@@ -21,6 +21,10 @@ class HichhikeSerializer(serializers.ModelSerializer):
 class JoinRequestsSerializer(serializers.ModelSerializer):
     hichhike_id = serializers.IntegerField(source='hichhike.pk', required=False)
     hichhike_capacity = serializers.IntegerField(source='hichhike.fellow_traveler_num', required=False)
+    hichhike_source = serializers.CharField(source='hichhike.source', required=False)
+    hichhike_destination = serializers.CharField(source='hichhike.destination', required=False)
+    hichhike_description = serializers.CharField(source='hichhike.description', required=False)
+    hichhike_trip_time = serializers.DateTimeField(source='hichhike.trip_time', required=False)
     passenger_id = serializers.IntegerField(source='passenger.pk', required=False)
     passenger_firstname = serializers.CharField(source='passenger.firstname', required=False)
     passenger_lastname = serializers.CharField(source='passenger.lastname', required=False)
@@ -29,12 +33,18 @@ class JoinRequestsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JoinRequest
-        fields = ('id', 'hichhike_id', 'hichhike_capacity', 'passenger_id', 'passenger_firstname', 'passenger_lastname'
+        fields = ('id', 'hichhike_id', 'hichhike_capacity', 'passenger_id', 'passenger_firstname', 'passenger_lastname',
+                  'hichhike_source', 'hichhike_destination',
+                  'hichhike_description', 'hichhike_trip_time'
                   , 'passenger_username', 'passenger_profile_picture')
 
 
 class ParticipantsDriverSerializer(serializers.ModelSerializer):
     hichhike_id = serializers.IntegerField(source='hichhike.pk', required=False)
+    hichhike_source = serializers.CharField(source='hichhike.source', required=False)
+    hichhike_destination = serializers.CharField(source='hichhike.destination', required=False)
+    hichhike_description = serializers.CharField(source='hichhike.description', required=False)
+    hichhike_trip_time = serializers.DateTimeField(source='hichhike.trip_time', required=False)
     passenger_id = serializers.IntegerField(source='passenger.pk', required=False)
     passenger_firstname = serializers.CharField(source='passenger.firstname', required=False)
     passenger_lastname = serializers.CharField(source='passenger.lastname', required=False)
@@ -42,12 +52,18 @@ class ParticipantsDriverSerializer(serializers.ModelSerializer):
     passenger_profile_picture = serializers.ImageField(source='passenger.profile_picture', required=False)
     class Meta:
         model = JoinRequest
-        fields = ('id', 'hichhike_id', 'passenger_id', 'passenger_firstname', 'passenger_lastname'
+        fields = ('id', 'hichhike_id', 'passenger_id', 'passenger_firstname', 'passenger_lastname',
+                  'hichhike_source', 'hichhike_destination',
+                  'hichhike_description', 'hichhike_trip_time'
                   , 'passenger_username', 'passenger_profile_picture')
 
 
 class ParticipantsPassengerSerializer(serializers.ModelSerializer):
     hichhike_id = serializers.IntegerField(source='hichhike.pk', required=False)
+    hichhike_source = serializers.CharField(source='hichhike.source', required=False)
+    hichhike_destination = serializers.CharField(source='hichhike.destination', required=False)
+    hichhike_description = serializers.CharField(source='hichhike.description', required=False)
+    hichhike_trip_time = serializers.DateTimeField(source='hichhike.trip_time', required=False)
     hichhike_capacity = serializers.IntegerField(source='hichhike.fellow_traveler_num', required=False)
     hichhike_creator = serializers.IntegerField(source='hichhike.creator.pk', required=False)
     hichhike_creator_username = serializers.CharField(source='hichhike.creator.username', required=False)
@@ -58,6 +74,7 @@ class ParticipantsPassengerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JoinRequest
-        fields = ('id', 'hichhike_id', 'hichhike_creator',
+        fields = ('id', 'hichhike_id', 'hichhike_creator','hichhike_source', 'hichhike_destination',
+                  'hichhike_description', 'hichhike_trip_time',
                   'hichhike_capacity', 'hichhike_creator_username', 'hichhike_creator_firstname'
                   , 'hichhike_creator_lastname', 'hichhike_creator_profile_picture')
